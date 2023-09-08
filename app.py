9eb370d4b75dbf1c87d66751e7d309ac91eb80ff
ryan-szalaty <ryan.szalaty@gmail.com>
2023-09-08 09:10:22 +0900

    Completed mailing feature.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, render_template, request
+import os, secrets
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 
 #Enable environment 
 load_dotenv()
+
+#Configure sessions key
+secret_key = secrets.token_hex(16)
 
 #Configure Flask Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -16,6 +19,7 @@ app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get("EMAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.environ.get("EMAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = 'Coffee Lover\'s Club'
+app.config['SECRET_KEY'] = secret_key
 
 mail = Mail(app)
 
@@ -37,10 +41,18 @@ def send_mail():
     recipient_email = request.form["email"]
 
     msg = Message("Coffee Lover's Club: Thank You!", recipients=[recipient_email])
-    msg.html = "<p>Check out the creator of Coffee Lover's Club <a href='https://ryan-szalaty.github.io'>here</a>.</p>"
+    msg.body = "Thank you for checking out Coffee Lover's Club! We are thrilled to have you!\n\nCoffee enthusiasts are similar no matter where they come from: They have a keen sense of refined taste and know what they want. Mild, medium, dark...there's something for everyone.\n\nCheck out the creator of Coffee Lover's Club at https://ryan-szalaty.github.io.\n\nHave a great day!"
 
     try:
         mail.send(msg)
-        return f"Email sent successfully! Please check your inbox at {recipient_email}."
-    except Exception as error:
-        return f"Email not sent. Error: {str(error)}."
+        session["flash_message"] = {
+            "message" : f"Email sent successfully! Please check your inbox at {recipient_email}.",
+            "category" : "success"
+        }
+        return redirect("/")
+    except Exception:
+        session["flash_message"] = {
+            "message" : f"Email not sent. Please check your email address and try again. If this error persists, please contact us.",
+            "category" : "warning"
+        }
+        return redirect(url_for("about"))
