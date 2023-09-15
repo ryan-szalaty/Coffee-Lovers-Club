5d77f2164ed6b040c11ad976a62b29e342e6fd6b
ryan-szalaty <ryan.szalaty@gmail.com>
2023-09-15 10:06:43 +0900

    Enabled hamburger menu.

--- static/dev-coffee.js
+++ static/dev-coffee.js
@@ -4,16 +4,45 @@ const espressoSection =  document.querySelector(".coffee-tips-espresso");
 const frothingSection = document.querySelector(".coffee-tips-froth");
 const clickToLearn = document.getElementById("click-to-learn");
 
-frothingButton.addEventListener("click", () => {
-    frothingSection.classList.add("on");
-    frothingSection.classList.remove("off");
-    espressoSection.classList.add("off");
-    espressoSection.classList.remove("on");
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
+
+function closeMenu() {
+    menu.classList.remove("show-menu");
+    overlay.classList.remove("show-overlay");
+}
+
+overlay.addEventListener("click", (event) => {
+    if (event.target === overlay) {
+        closeMenu();
+    }
 });
 
-espressoButton.addEventListener("click", () => {
-    espressoSection.classList.add("on");
-    espressoSection.classList.remove("off");
-    frothingSection.classList.add("off");
-    frothingSection.classList.remove("on");
-})
+if (frothingButton && espressoButton) {
+    frothingButton.addEventListener("click", () => {
+        frothingSection.classList.add("on");
+        frothingSection.classList.remove("off");
+        espressoSection.classList.add("off");
+        espressoSection.classList.remove("on");
+    });
+    
+    espressoButton.addEventListener("click", () => {
+        espressoSection.classList.add("on");
+        espressoSection.classList.remove("off");
+        frothingSection.classList.add("off");
+        frothingSection.classList.remove("on");
+    });
+}
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("show-menu");
+    overlay.classList.toggle("show-overlay");
+});
+
+menu.querySelectorAll("a").forEach((item) => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("show-menu");
+        overlay.classList.remove("show-overlay");
+    });
+});
